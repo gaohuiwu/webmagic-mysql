f228e735bbacffa100665a897f2c7f5d8f96957e
gaohw <ghw0614@126.com>
2018-09-29 11:00:29 +0800

    关于爬虫项目的第一次提交springboot脚手架

---
+++ src/main/java/com/ghw/webmagicmysql/WebmagicMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.ghw.webmagicmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmagicMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmagicMysqlApplication.class, args);
+	}
+}
